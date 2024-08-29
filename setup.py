9164fd4ff5cb7cfdd5d1e0431ecee97828fd1f11
Gustavo Inostroza <gusinostrozar@gmail.com>
2024-08-29 01:04:17 -0400

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PgDbToolkit",
-    version="0.1.0",
+    version="0.1.1",
     author="Gustavo Inostroza",
     author_email="gusinostrozar@gmail.com",
     description="A package for managing PostgreSQL database operations",
